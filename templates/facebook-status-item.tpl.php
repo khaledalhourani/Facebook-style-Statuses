e0110a273402ff84c7944151b8af361ab22b3b52
screamwork <screamwork77@yahoo.de>
2011-05-15 17:15:30 +0200

    admin.inc fixed

--- templates/facebook-status-item.tpl.php
+++ templates/facebook-status-item.tpl.php
@@ -34,16 +34,25 @@
  */
 ?>
 
-<div id="facebook-status-item-
-<?php
-echo $sid;
-?>
-" class="facebook-status-item facebook-status-type-
-<?php
-echo $type;
-?>
-<?php
-if ($self) :
-?>
- facebook-status-self-update
-<?php
+<div id="facebook-status-item-<?php echo $sid; ?>" class="facebook-status-item facebook-status-type-<?php echo $type; ?><?php if ($self): ?> facebook-status-self-update<?php endif; ?><?php if ($page): ?> facebook-status-page<?php endif; ?>">
+  <?php if ($sender_picture): ?>
+    <div class="facebook-status-sender-picture"><?php echo $sender_picture; ?></div>
+  <?php endif; ?>
+  <span class="facebook-status-sender"><?php echo $sender_name; ?></span>
+  <?php if ($type == 'user' && !$self): ?>
+    &raquo; <span class="facebook-status-recipient"><?php echo $recipient_link; ?></span>
+  <?php endif; ?>
+  <span class="facebook-status-content"><?php echo $message; ?></span>
+  <div class="facebook-status-details">
+    <span class="facebook-status-time"><?php echo $created; ?></span>
+    <?php if ($meta): ?>
+      <span class="facebook-status-meta"><?php echo $meta; ?></span>
+    <?php endif; ?>
+    <?php if ($links): ?>
+      <div class="facebook-status-links"><?php echo $links; ?></div>
+    <?php endif; ?>
+  </div>
+  <?php if (!empty($comments)): ?>
+    <div class="facebook-status-comments"><?php echo $comments; ?></div>
+  <?php endif; ?>
+</div>
