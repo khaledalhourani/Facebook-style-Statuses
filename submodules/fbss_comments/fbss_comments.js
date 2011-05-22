2a55254c9b4ab955561e387ee03a590a7b27b34e
screamwork <screamwork77@yahoo.de>
2011-05-22 18:46:05 +0200

    can't find edit bug

--- submodules/fbss_comments/fbss_comments.js
+++ submodules/fbss_comments/fbss_comments.js
@@ -2,17 +2,17 @@
 
   Drupal.behaviors.fbss_comments = {
     attach: function (context, settings) {
-      $('.fbss-comments-show-comment-form').one('click', function() {
+      $('.fbss-comments-show-comment-form', context).one('click', function() {
         $(this).hide();
         $('#'+ this.id +' + div').show();
         return false;
       });
-      $('.fbss-comments-show-comment-form-inner').one('click', function() {
+      $('.fbss-comments-show-comment-form-inner', context).one('click', function() {
         $(this).hide();
         $('#'+ this.id +' + div').show();
         return false;
       });
-      $('a.fbss-comments-show-comments').one('click', function() {
+      $('a.fbss-comments-show-comments', context).one('click', function() {
         $(this).hide();
         $('#'+ this.id +' ~ div.fbss-comments-hide').show();
         return false;
