d7c0542f80aecf288dfee634d6c365fa8c4a5d7b
screamwork <screamwork77@yahoo.de>
2011-05-22 05:08:01 +0200

    comments becoming good

--- submodules/fbss_comments/fbss_comments.js
+++ submodules/fbss_comments/fbss_comments.js
@@ -1,17 +1,23 @@
-Drupal.behaviors.fbss_comments = function (context) {
-  $('.fbss-comments-show-comment-form').one('click', function() {
-    $(this).hide();
-    $('#'+ this.id +' + div').show();
-    return false;
-  });
-  $('.fbss-comments-show-comment-form-inner').one('click', function() {
-    $(this).hide();
-    $('#'+ this.id +' + div').show();
-    return false;
-  });
-  $('a.fbss-comments-show-comments').one('click', function() {
-    $(this).hide();
-    $('#'+ this.id +' ~ div.fbss-comments-hide').show();
-    return false;
-  });
-}
+(function ($) {
+
+  Drupal.behaviors.fbss_comments = {
+    attach: function (context, settings) {
+      $('.fbss-comments-show-comment-form').one('click', function() {
+        $(this).hide();
+        $('#'+ this.id +' + div').show();
+        return false;
+      });
+      $('.fbss-comments-show-comment-form-inner').one('click', function() {
+        $(this).hide();
+        $('#'+ this.id +' + div').show();
+        return false;
+      });
+      $('a.fbss-comments-show-comments').one('click', function() {
+        $(this).hide();
+        $('#'+ this.id +' ~ div.fbss-comments-hide').show();
+        return false;
+      });
+    }
+  }
+
+})(jQuery);
