79e412e52d8cf94d07caf82e56ea6a034a56cf0c
screamwork <screamwork77@yahoo.de>
2011-05-26 09:58:50 +0200

    template conditional statements

--- submodules/fbss_comments/templates/fbss-comments-item.tpl.php
+++ submodules/fbss_comments/templates/fbss-comments-item.tpl.php
@@ -7,22 +7,26 @@
 ?>
 
 <div class="<?php print $classes; ?>">
-  <div class="fbss-comments-userpic"><?php print $userpic; ?></div>
-  <div class="fbss-comments-author"><?php print $username; ?></div>
-  <div class="fbss-comments-text"><?php print $comment_text; ?></div>
-  <div class="fbss-comments-closure">
-    <span class="fbss-comments-created">
-      <?php print $facebook_time; ?>
-    </span>
-    <?php if (isset($edit_link)):?>
-      <span class="fbss-comments-edit-delete">
-        <?php print $edit_link; ?>
-      </span>
-    <?php endif;?>
-    <?php if (isset($delete_link)):?>
-      <span class="fbss-comments-edit-delete">
-        <?php print $delete_link; ?>
-      </span>
-    <?php endif;?>
-  </div>
+  <?php if (isset($userpic)): ?>
+    <div class="fbss-comments-userpic"><?php print $userpic; ?></div>
+  <?php endif; ?>
+  <?php if (isset($username)): ?>
+    <div class="fbss-comments-author"><?php print $username; ?></div>
+  <?php endif; ?>
+  <?php if (isset($comment_text)): ?>
+    <div class="fbss-comments-text"><?php print $comment_text; ?></div>
+  <?php endif; ?>
+  <?php if (isset($facebook_time) || isset($delete_link) || isset($edit_link)): ?>
+	  <div class="fbss-comments-closure">
+	    <?php if (isset($facebook_time)): ?>
+	      <span class="fbss-comments-created"><?php print $facebook_time; ?></span>
+	    <?php endif; ?>
+	    <?php if (isset($edit_link)):?>
+	      <span class="fbss-comments-edit-delete"><?php print $edit_link; ?></span>
+	    <?php endif; ?>
+	    <?php if (isset($delete_link)):?>
+	      <span class="fbss-comments-edit-delete"><?php print $delete_link; ?></span>
+	    <?php endif; ?>
+	  </div>
+  <?php endif; ?>
 </div>
